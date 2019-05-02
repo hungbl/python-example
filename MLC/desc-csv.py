99d6fccd295d03f87647327b76e0bbd222432909
Hung Bui <hungbl6572@massgenie.com>
2019-05-02 17:27:36 +0700

    Python Machine Learning example

---
+++ MLC/desc-csv.py
@@ -0,0 +1,9 @@
+# Statistical Summary
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+description = data.describe()
+head = data.head()
+print(head)
+print(description)
--- desc-csv.py
+++
@@ -1,9 +0,0 @@
-# Statistical Summary
-import pandas
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = pandas.read_csv(url, names=names)
-description = data.describe()
-head = data.head()
-print(head)
-print(description)
